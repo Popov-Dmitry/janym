4e66b871cb8a4b7c245fc277f08390cd408379cb
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-12-04 20:58:00 +0900

    Init styles, added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,29 @@ module.exports = {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "var(--primary)",
       },
+      fontFamily: {
+        "sf-pro": "var(--font-sf-pro)",
+        "be-vietnam-pro": "var(--font-be-vietnam-pro)",
+      },
+      fontSize: {
+        "16px": 16,
+        "20px": 20,
+        "32px": 32,
+        "54px": 54,
+        "58px": 58
+      },
+      lineHeight: {
+        "17px": 17,
+        "21px": 21,
+        "23px": 23,
+        "34px": 34,
+        "54px": 54,
+        "58px": 58,
+        "60px": 60,
+        "68px": 68
+      }
     },
   },
   plugins: [],
