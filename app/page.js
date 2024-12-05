4b09d5c865b35b8a7675da0af6014fd5d52a9668
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-12-05 22:23:44 +0900

    Added homepage first block

--- app/page.js
+++ app/page.js
@@ -1,20 +1,31 @@
 import { productsPreviewMock } from "@/data";
 import ProductPreview from "@/app/components/product-preview/ProductPreview";
-import styles from "./page.module.scss";;
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <div>
-      {/*<div>*/}
-      {/*  <div>*/}
-      {/*    <div>FW’24</div>*/}
-      {/*    <div>Double Wrap Bracelet</div>*/}
-      {/*    <div>*/}
-      {/*      <div>Janym Sol</div>*/}
-      {/*      <div>FW’24</div>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+    <div className={styles.container}>
+      <div>
+        <div className={styles.fw}>
+          <div className={styles.fwTitle}>FW’24</div>
+          <div className={styles.fwSubtitle}>Double Wrap Bracelet</div>
+          <div className={styles.fwCaption}>
+            <div>Janym Sol</div>
+            <div>FW’24</div>
+          </div>
+        </div>
+        <img src="/aral-sea-collection.png" alt="" className={styles.fwImage} />
+        <div className={styles.fwBottom}>
+          <div className={styles.fwBottomCaption}>
+            <div>FW’</div>
+            <div>24</div>
+          </div>
+          <div className={styles.fwBottomText}>
+            &#34;A brand reinterpreting Kazakh traditions in a modern context. Each collection reflects the nature and
+            history of Kazakhstan, with the first one responding to the tragedy of the Aral Sea.&#34;
+          </div>
+        </div>
+      </div>
 
       <div className={styles.products}>
         {productsPreviewMock.map((product) => (
