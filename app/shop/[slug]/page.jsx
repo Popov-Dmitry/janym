9982e31a4f3cadb503e9f0eddb11e95d532eddbd
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-12-09 17:38:47 +0900

    Added product page

---
+++ app/shop/[slug]/page.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./product.module.scss";
+import { productMock } from "@/data";
+import Button from "@/components/button/Button";
+
+const Product = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageWrapper}>
+        <img src={productMock.images[0]} alt={productMock.title} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.title}>{productMock.title}</div>
+        <div className={styles.description}>{productMock.description}</div>
+        <div className={styles.specification}>
+          <div>{productMock.material}</div>
+          <div>{productMock.color}</div>
+          <div>{productMock.size}</div>
+        </div>
+        <div className={styles.price}>${productMock.price}</div>
+        <div className={styles.bottom}>
+          <div className={styles.caption}>
+            Shipping: Worldwide shipping is free for orders of $ 350 and above. Delivery is carried out within 5-14 days
+            from the date of confirmation of the order.
+          </div>
+          <div className={styles.divider} />
+          <Button text="Add to bag" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- data.js
+++ data.js
@@ -10,3 +10,17 @@ export const productsPreviewMock = [
     price: 600
   }
 ];
+
+export const productMock = {
+  slug: "double-wrap-bracelet",
+  title: "Double Wrap Bracelet",
+  description: "Handcrafted from black leather and silver-tone metal, angular bracelet with a combination of ornaments.",
+  material: "Silver, leather",
+  color: "Silver/Black",
+  size: "One size",
+  price: 600,
+  images: [
+    "/mock/product/1.png",
+    "/mock/product/2.png"
+  ]
+};
