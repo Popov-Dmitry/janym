4160d9b8087b1f5f51e11fc02cbb33c7717bdb1e
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-12-13 22:47:27 +0900

    Connected product page to db

--- utils/db-requests.js
+++ utils/db-requests.js
@@ -2,7 +2,17 @@ import { createClient } from "@/utils/supabase";
 
 export const getPreviews = async () => {
   const supabase = await createClient();
-  const { data } = await supabase.from("products").select("slug, cover, title, price");
+  const { data } = await supabase.from("products")
+    .select("slug, cover, title, price");
 
   return data;
 }
+
+export const getProduct = async (slug) => {
+  const supabase = await createClient();
+  const { data } = await supabase.from("products")
+    .select("title, description, material, color, size, price, images")
+    .eq("slug", slug);
+
+  return data[0];
+};
