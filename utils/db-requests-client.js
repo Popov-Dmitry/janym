e74978eb13c869fc4f25cebda0b695a90d75ef2c
Popov-Dmitry <popov.dmitry.dev@gmail.com>
2024-12-17 20:09:22 +0900

    Added bag page mobile version

--- utils/db-requests-client.js
+++ utils/db-requests-client.js
@@ -3,7 +3,7 @@ import { createSupabaseBrowserClient } from "@/utils/supabase-browser-client";
 export const getProductsFromCart = async (productsSlug) => {
   const supabase = await createSupabaseBrowserClient();
   const { data } = await supabase.from("products")
-    .select("slug, cover, title, price")
+    .select("slug, cover, title, price, size, material")
     .in("slug", productsSlug);
 
   return data;
